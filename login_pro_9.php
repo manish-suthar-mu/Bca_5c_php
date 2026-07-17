028f54d114b64725a692504d1981d05edaf52d56
manishsuthar94 <manishsutharr94@gmail.com>
2026-07-17 10:40:10 +0530

    rename

--- login_pro_9.php
+++ login_pro_9.php
@@ -4,7 +4,7 @@
 
 <h2>Login Form</h2>
 
-<form action="check.php" method="post">
+<form action="check_pro_9.php" method="post">
     User ID: <input type="text" name="userid"><br><br>
     Password: <input type="password" name="password"><br><br>
     <input type="submit" value="Login">
--- save_pro_9.php
+++ save_pro_9.php
@@ -5,5 +5,5 @@ $_SESSION["userid"] = $_POST["userid"];
 $_SESSION["password"] = $_POST["password"];
 
 echo "Registration Successful!<br><br>";
-echo "<a href='login.php'>Go to Login</a>";
+echo "<a href='login_pro_9.php'>Go to Login</a>";
 ?>
