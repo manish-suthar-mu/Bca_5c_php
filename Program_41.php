f19830623d5f7118213107607ee884f4a383d010
manishsuthar94 <manishsutharr94@gmail.com>
2026-08-14 10:08:06 +0530

    41

--- Program_41.php
+++ Program_41.php
@@ -10,6 +10,7 @@ $("#calc").click(function() {
     var a = Number($("#a").val());
     var b = Number($("#b").val());
 
+    
     $("#result").html(
         "Addition = " + (a + b) + "<br>" +
         "Subtraction = " + (a - b) + "<br>" +
