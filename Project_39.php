a223f75096a6e6d40631aea29cf68fbd9f92b21d
manishsuthar94 <manishsutharr94@gmail.com>
2026-08-14 10:04:09 +0530

    39

--- Project_39.php
+++ Project_39.php
@@ -1,1 +1,15 @@
-git 
+<input type="text" class="num" value="20">
+<input type="text" class="num" value="10">
+
+<button class="sub">Subtract</button>
+
+<p id="result"></p>
+
+<script>
+$(".sub").click(function() {
+    var a = Number($(".num").eq(0).val());
+    var b = Number($(".num").eq(1).val());
+
+    $("#result").text("Result = " + (a - b));
+});
+</script>
